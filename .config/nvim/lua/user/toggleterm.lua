09a3a6024ff4c4fb4990939ce3691b9b30f2d128
Juan Pedro Martin <juamp_m@hotmail.com>
2025-02-16 15:55:41 +0100

    refactor(vim): change toggleterm horizontal width to 20%

--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -5,7 +5,7 @@ local M = {
 
 function M.config()
   local execs = {
-    { nil, "<M-1>", "Horizontal Terminal", "horizontal", 0.3 },
+    { nil, "<M-1>", "Horizontal Terminal", "horizontal", 0.2 },
     { nil, "<M-2>", "Vertical Terminal", "vertical", 0.4 },
     { nil, "<M-3>", "Float Terminal", "float", nil },
   }

a10ce13573af46a3daa5cd46aee7acf0837ad7bc
Juan Pedro Martin <juamp_m@hotmail.com>
2025-03-24 16:33:08 +0100

    add avante.nvim back and update it's config

--- .config/nvim/lua/user/extras/avante.lua
+++ .config/nvim/lua/user/extras/avante.lua
@@ -1,25 +1,74 @@
 local M = {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  version = false,
   lazy = false,
   opts = {},
-  build = ":AvanteBuild",
+  build = "make",
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    --- Optional but recommended dependencies
+    "echasnovski/mini.pick",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "Avante" },
+      },
+      ft = { "Avante" },
+    },
   },
 }
 
 function M.config()
   require("avante").setup {
     ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-    provider = "claude", -- Recommend using Claude
+    provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    auto_suggestions_provider = "claude",
+    cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20240620",
+      model = "claude-3-5-sonnet-20241022",
       temperature = 0,
       max_tokens = 4096,
+    },
+    dual_boost = {
+      enabled = false,
+      first_provider = "openai",
+      second_provider = "claude",
+      prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
+    },
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+      minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+      enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+      enable_claude_text_editor_tool_mode = false, -- Whether to enable Claude Text Editor Tool Mode.
     },
     mappings = {
       --- @class AvanteConflictMappings
@@ -32,6 +81,12 @@ function M.config()
         next = "]x",
         prev = "[x",
       },
+      suggestion = {
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
       jump = {
         next = "]]",
         prev = "[[",
@@ -40,16 +95,44 @@ function M.config()
         normal = "<CR>",
         insert = "<C-s>",
       },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        retry_user_request = "r",
+        edit_user_request = "e",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+        remove_file = "d",
+        add_file = "@",
+        close = { "<Esc>", "q" },
+        close_from_input = nil, -- e.g., { normal = "<Esc>", insert = "<C-d>" }
+      },
     },
-    hints = { enabled = true },
+    hints = { enabled = false },
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
       position = "right", -- the position of the sidebar
       wrap = true, -- similar to vim.o.wrap
       width = 30, -- default % based on available width
       sidebar_header = {
-        align = "center", -- left, center, right for title
-        rounded = true,
+        enabled = true, -- true, false to enable/disable the header
+        align = "left", -- left, center, right for title
+        rounded = false,
+      },
+      input = {
+        prefix = "> ",
+        height = 8, -- Height of the input window in vertical layout
+      },
+      edit = {
+        border = "rounded",
+        start_insert = true, -- Start insert mode when opening the edit window
+      },
+      ask = {
+        floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+        start_insert = true, -- Start insert mode when opening the ask window
+        border = "rounded",
+        ---@type "ours" | "theirs"
+        focus_on_apply = "ours", -- which diff to focus after applying
       },
     },
     highlights = {
@@ -64,6 +147,11 @@ function M.config()
       autojump = true,
       ---@type string | fun(): any
       list_opener = "copen",
+      override_timeoutlen = 500,
+    },
+    suggestion = {
+      debounce = 600,
+      throttle = 600,
     },
   }
 end

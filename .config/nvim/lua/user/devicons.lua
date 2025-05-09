c258f9625db7e738d3c6da690200ea27e67c0548
Juan Pedro Martin <juamp_m@hotmail.com>
2025-05-09 15:00:42 +0200

    add Go lsp and update icons

--- .config/nvim/lua/user/devicons.lua
+++ .config/nvim/lua/user/devicons.lua
@@ -1,10 +1,19 @@
 local M = {
-	"nvim-tree/nvim-web-devicons",
-	event = "VeryLazy",
+  "nvim-tree/nvim-web-devicons",
+  event = "VeryLazy",
 }
 
 function M.config()
-	require("nvim-web-devicons")
+  require("nvim-web-devicons").setup {
+    override = {
+      go = {
+        icon = "󰟓",
+        color = "#00ADD8",
+        cterm_color = "38",
+        name = "Go",
+      },
+    },
+  }
 end
 
 return M

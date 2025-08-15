01e9a2e5aa9df90b773a4f1d4214710c1009901a
Juan Pedro Martin <juamp_m@hotmail.com>
2025-08-15 02:57:59 +0200

    feat(nvim): add treesitter-context plugin

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -5,6 +5,22 @@ local M = {
     {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = {
+        enable = true,
+        multiwindow = false,
+        max_lines = 1,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      },
+    },
   },
 }
 

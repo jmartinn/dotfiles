88f5b1e8f28c55180bcd4877d799e14d3e30ecca
Juan Pedro Martin <juamp_m@hotmail.com>
2025-12-17 00:45:19 +0100

    update colorscheme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -12,6 +12,11 @@ local M = {
 function M.config()
   require("tokyonight").setup {
     style = "night",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   }
 
   -- require("rose-pine").setup {
